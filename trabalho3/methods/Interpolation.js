4ea21f215bcbc63c6159d8bd29a7d0b53a344384
Gustavo <gustavo@bgcbrasil.com.br>
2021-08-23 17:10:57 -0300

    interpolação working

--- trabalho3/methods/Interpolation.js
+++ trabalho3/methods/Interpolation.js
@@ -1,5 +1,22 @@
-function Interpolation() {
+function Interpolation({ n, mainX, pairs }) {
+  let total = 0;
+  for (const { x: currentX, y: currentY } of pairs) {
+    let upSum = 1;
+    let downSum = 1;
+    for (const { x } of pairs) {
+      if (x === currentX) continue;
+      upSum *= (mainX - x);
+      downSum *= (currentX - x);
+    }
 
+    total += currentY*(upSum/downSum);
+  }
+
+  console.log({ total });
+
+  return {
+    y: total,
+  };
 }
 
 module.exports = Interpolation;
